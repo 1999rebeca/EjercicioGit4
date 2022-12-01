49207c21d45cd50946c39c1e1ffa2e0a5bad0abb
rebeca <rebecabargiela@gmail.com>
2022-12-01 19:15:43 +0100

    diseñamos las pruebas de esta funcionalidad

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,13 @@
 import java.util.Calendar;
 import java.util.Date;
-
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello world!");
-=======
->>>>>>> d93d81c3da347cdd904fcc5a78cf5b9693ec3150
-
-        Persona persona = new Persona();
-        persona.nombre = "Pepe Pérez";
-        persona.edad = 31;
-        persona.fechaContratacion = new Date(90, Calendar.FEBRUARY, 1);
-
-        System.out.println("Se ha registrado a " + persona.nombre + " (" + persona.edad + " años; contratado en 19" + persona.fechaContratacion.getYear() + ")");
-<<<<<<< HEAD
-=======
 
         Empleado p = new Empleado("Pepe Pérez", 31, new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
->>>>>>> d93d81c3da347cdd904fcc5a78cf5b9693ec3150
+
+
+        p.nombre = "Manolo"; // Cambio de nombre. No se debe permitir
+        p.fechaContratacion = new Date(99, Calendar.FEBRUARY, 1); // Cambio de fecha. No se debe permitir
     }
 }
